ef4bbbbeee3d4513ccc7c37658cad3cfb5c7f341
toxicmix <72023909+toxicmix@users.noreply.github.com>
2023-08-14 21:04:24 -0500

    finished the rest of the code cuz im just that fuckin good

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
 const storage = require('../db/storage');
 
+router.post('/notes', (req, res) => {
+    storage
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch((err) => res.status(500).json(err));
+  });
+  
+  router.delete('/notes/:id', (req, res) => {
+    storage
+      .removeNote(req.params.id)
+      .then(() => res.json({ ok: true }))
+      .catch((err) => res.status(500).json(err));
+  });
+
+router.get('/notes', (req, res) => {
+    storage
+      .getNotes()
+      .then((notes) => {
+        return res.json(notes);
+      })
+      .catch((err) => res.status(500).json(err));
+  });  
+  module.exports = router;
